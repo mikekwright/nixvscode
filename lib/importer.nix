a961bd0f53bc0ae75b04fdbd95c1d230f723e459
Michael Wright <mkwright@gmail.com>
2025-07-06 16:38:05 -0600

    Updated the package to include a desktop

--- lib/importer.nix
+++ lib/importer.nix
@@ -82,99 +82,119 @@ in {
           #pkgs.vscode-marketplace.golang.go
         ]) ++ fullModule.vscodeExtensions;
       };
-    in pkgs.writeShellApplication {
-      name = "code";
-      runtimeInputs = [
-        #pkgs.vscode
-        vscode-wrapper
-      ] ++ modulePackages;
+
+      vscodeApp = pkgs.writeShellApplication {
+        name = "code";
+        runtimeInputs = [
+          #pkgs.vscode
+          vscode-wrapper
+        ] ++ modulePackages;
 
 
-      #
-      # NOTE: When using vscode, you need to set it up in a writable directly for the user directory
-      #   this is because vscode creates a number of other files, and if it can't create those files
-      #   it will just crash.
-      #
-      text = /*shell*/ ''
-        set +u
-        if [[ -n $VSCODE_DEBUG ]]; then
-          echo 'VSCode Settings:'
-          cat ${settingsJson}
+        #
+        # NOTE: When using vscode, you need to set it up in a writable directly for the user directory
+        #   this is because vscode creates a number of other files, and if it can't create those files
+        #   it will just crash.
+        #
+        text = /*shell*/ ''
+          set +u
+          if [[ -n $VSCODE_DEBUG ]]; then
+            echo 'VSCode Settings:'
+            cat ${settingsJson}
 
-          echo 'VSCode Keybindings:'
-          cat ${keybindingsJson}
+            echo 'VSCode Keybindings:'
+            cat ${keybindingsJson}
 
-          echo 'VSCode Extensions:'
-          cat ${extensionsJson}
+            echo 'VSCode Extensions:'
+            cat ${extensionsJson}
 
-          echo 'Startup scripts:'
-          echo '${scriptText}'
+            echo 'Startup scripts:'
+            echo '${scriptText}'
 
-          echo '-------------------------'
-          echo "VSCode settings file path: ${settingsJson}"
-          echo "VSCode keybindings file path: ${keybindingsJson}"
-          echo "VSCode user data directory: ${vscodeUserDataDir}"
-        elif [[ -n $VSCODE_TEMP ]]; then
-          ${scriptText}
+            echo '-------------------------'
+            echo "VSCode settings file path: ${settingsJson}"
+            echo "VSCode keybindings file path: ${keybindingsJson}"
+            echo "VSCode user data directory: ${vscodeUserDataDir}"
+          elif [[ -n $VSCODE_TEMP ]]; then
+            ${scriptText}
 
-          # Create a temporary directory for VSCode user data
-          TEMP_USER_DATA_DIR=$(mktemp -d)
-          echo "using temporary vscode user data directory: $TEMP_USER_DATA_DIR"
+            # Create a temporary directory for VSCode user data
+            TEMP_USER_DATA_DIR=$(mktemp -d)
+            echo "using temporary vscode user data directory: $TEMP_USER_DATA_DIR"
 
-          # Make sure the User portion of the directory exists (path for settings)
-          mkdir -p "$TEMP_USER_DATA_DIR/User"
+            # Make sure the User portion of the directory exists (path for settings)
+            mkdir -p "$TEMP_USER_DATA_DIR/User"
 
-          # copy our settings to the temporary directory
-          cp ${settingsJson} "$TEMP_USER_DATA_DIR/User/settings.json"
-          chmod 444 "$TEMP_USER_DATA_DIR/User/settings.json"
+            # copy our settings to the temporary directory
+            cp ${settingsJson} "$TEMP_USER_DATA_DIR/User/settings.json"
+            chmod 444 "$TEMP_USER_DATA_DIR/User/settings.json"
 
-          # copy our keybindings to the temporary directory
-          cp ${keybindingsJson} "$TEMP_USER_DATA_DIR/User/keybindings.json"
-          chmod 444 "$TEMP_USER_DATA_DIR/User/keybindings.json"
+            # copy our keybindings to the temporary directory
+            cp ${keybindingsJson} "$TEMP_USER_DATA_DIR/User/keybindings.json"
+            chmod 444 "$TEMP_USER_DATA_DIR/User/keybindings.json"
 
-          # Run VSCode with the Nix-managed user data directory
-          echo "${pkgs.vscode}/bin/code --user-data-dir=$TEMP_USER_DATA_DIR"
-          ${vscode-wrapper}/bin/code --user-data-dir="$TEMP_USER_DATA_DIR" "$@"
+            # Run VSCode with the Nix-managed user data directory
+            echo "${pkgs.vscode}/bin/code --user-data-dir=$TEMP_USER_DATA_DIR"
+            ${vscode-wrapper}/bin/code --user-data-dir="$TEMP_USER_DATA_DIR" "$@"
 
-          if [[ -z $SKIP_VSCODE_CLEAN ]]; then
-            # Clean up the temporary directory when VSCode exits
-            #  TODO: We can't clean up the directories as vscode runs in another thread
-            #  and this kills it to quickly
-            #rm -rf "$TEMP_USER_DATA_DIR"
-            echo "Cant delete right now"
+            if [[ -z $SKIP_VSCODE_CLEAN ]]; then
+              # Clean up the temporary directory when VSCode exits
+              #  TODO: We can't clean up the directories as vscode runs in another thread
+              #  and this kills it to quickly
+              #rm -rf "$TEMP_USER_DATA_DIR"
+              echo "Cant delete right now"
+            else
+              echo "Ignored cleanup of temp directory $TEMP_USER_DATA_DIR"
+            fi;
           else
-            echo "Ignored cleanup of temp directory $TEMP_USER_DATA_DIR"
-          fi;
-        else
-          ${scriptText}
+            ${scriptText}
 
-          echo "Using VSCode for user home directory: $HOME/.vscode"
-          if [[ ! -d "$HOME/.config/Code" ]]; then
-            mkdir -p "$HOME/.config/Code/User"
+            echo "Using VSCode for user home directory: $HOME/.vscode"
+            if [[ ! -d "$HOME/.config/Code" ]]; then
+              mkdir -p "$HOME/.config/Code/User"
+            fi
+
+            # copy our settings to the temporary directory
+            if [[ -e "$HOME/.config/Code/User/settings.json" ]]; then
+              echo "Found existing settings.json, removing it"
+              rm -f "$HOME/.config/Code/User/settings.json.bak"
+              mv "$HOME/.config/Code/User/settings.json" "$HOME/.config/Code/User/settings.json.bak"
+            fi
+            cp ${settingsJson} "$HOME/.config/Code/User/settings.json"
+            chmod 444 "$HOME/.config/Code/User/settings.json"
+
+            # copy our keybindings to the temporary directory
+            if [[ -e "$HOME/.config/Code/User/keybindings.json" ]]; then
+              echo "Found existing keybindings.json, removing it"
+              rm -f "$HOME/.config/Code/User/keybindings.json.bak"
+              mv "$HOME/.config/Code/User/keybindings.json" "$HOME/.config/Code/User/keybindings.json.bak"
+            fi
+            cp ${keybindingsJson} "$HOME/.config/Code/User/keybindings.json"
+            chmod 444 "$HOME/.config/Code/User/keybindings.json"
+
+            # Run VSCode with the User's directory
+            echo "${pkgs.vscode}/bin/code --user-data-dir=$TEMP_USER_DATA_DIR"
+            ${vscode-wrapper}/bin/code --user-data-dir="$TEMP_USER_DATA_DIR" "$@"
           fi
+        '';
+      };
 
-          # copy our settings to the temporary directory
-          if [[ -e "$HOME/.config/Code/User/settings.json" ]]; then
-            echo "Found existing settings.json, removing it"
-            rm -f "$HOME/.config/Code/User/settings.json.bak"
-            mv "$HOME/.config/Code/User/settings.json" "$HOME/.config/Code/User/settings.json.bak"
-          fi
-          cp ${settingsJson} "$HOME/.config/Code/User/settings.json"
-          chmod 444 "$HOME/.config/Code/User/settings.json"
-
-          # copy our keybindings to the temporary directory
-          if [[ -e "$HOME/.config/Code/User/keybindings.json" ]]; then
-            echo "Found existing keybindings.json, removing it"
-            rm -f "$HOME/.config/Code/User/keybindings.json.bak"
-            mv "$HOME/.config/Code/User/keybindings.json" "$HOME/.config/Code/User/keybindings.json.bak"
-          fi
-          cp ${keybindingsJson} "$HOME/.config/Code/User/keybindings.json"
-          chmod 444 "$HOME/.config/Code/User/keybindings.json"
-
-          # Run VSCode with the User's directory
-          echo "${pkgs.vscode}/bin/code --user-data-dir=$TEMP_USER_DATA_DIR"
-          ${vscode-wrapper}/bin/code --user-data-dir="$TEMP_USER_DATA_DIR" "$@"
-        fi
+    in pkgs.stdenv.mkDerivation rec {
+      name = "code";
+      buildCommand = let
+        desktopEntry = pkgs.makeDesktopItem {
+          inherit name;
+          desktopName = "Visual Studio Code";
+          exec = "${vscodeApp}/bin/${name} %f";
+          terminal = true;
+        };
+      in ''
+        mkdir -p $out/bin
+        cp ${vscodeApp}/bin/${name} $out/bin
+        mkdir -p $out/share/applications
+        cp ${desktopEntry}/share/applications/${name}.desktop $out/share/applications/${name}.desktop
       '';
+      dontBuild = true;
     };
 }
+
