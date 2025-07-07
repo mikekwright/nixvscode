a18de44a6be38a3ada0aa381d2449929b7cf6a2b
Michael Wright <michael@level.co>
2025-07-07 10:20:32 -0600

    Added a missing extension for working with dlang

--- config/lsp/dlang.nix
+++ config/lsp/dlang.nix
@@ -5,6 +5,11 @@
     # Dlang main extension
     #    https://marketplace.visualstudio.com/items?itemName=webfreak.code-d
     vscode-marketplace.webfreak.code-d
+
+    # A testing UI solution that is a dependency of code-d (hopefully the code-d
+    #    will be updated to use the vscode built-in apis added in version 1.59)
+    #    https://marketplace.visualstudio.com/items?itemName=hbenl.vscode-test-explorer
+    vscode-marketplace.hbenl.vscode-test-explorer
   ];
 
   packages = with pkgs; [
